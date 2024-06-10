f8e3d0818f0e975a94beeaec8f874324c610d078
SuveenE <suveen.te1@gmail.com>
2024-06-10 23:14:45 +0800

    Fix build errors

--- src/app/country/[countryName]/page.tsx
+++ src/app/country/[countryName]/page.tsx
@@ -3,41 +3,24 @@ import geojsonData from "../../ne_110m_populated_places_simple.json";
 import { Feature } from "@/utils/types";
 
 export interface Props {
-    params: {
-      countryName: string;
-    };
-  }
-
-  const CountryHome = ({ params }: Props) => {
-    return (
-      <div className="flex flex-col w-full" >
-        {params.countryName.replace('%20', ' ')}
-      </div>
-    );
+  params: {
+    countryName: string;
   };
+}
+
+const CountryHome = ({ params }: Props) => {
+  return (
+    <div className="flex flex-col w-full">
+      {params.countryName.replace("%20", " ")}
+    </div>
+  );
+};
 
 export async function generateStaticParams() {
-    const { features } = await geojsonData;
-    if (features){
-      return features.map((feature: { properties: { sov0name: string } }) => ({
-        params: {
-          countryName: feature.properties.sov0name.replace(' ', '%20'), // URL encode the country name
-        },
-      }));
-    }
-    return [];
-  }
-  
-const getStaticPaths: GetStaticPaths = async () => {
-    const paths = (await generateStaticParams()).map((params: any) => ({
-      params,
-    }));
-  
-    return {
-      paths,
-      fallback: false, // or true if you want fallback behavior
-    };
-  };
-  
-  
-  export default CountryHome;
+  const countryNames = ['Singapore', 'Sri Lanka']; 
+  return countryNames.map((countryName) => ({
+    countryName: countryName.replace(' ', '%20'), 
+  }));
+}
+
+export default CountryHome;
