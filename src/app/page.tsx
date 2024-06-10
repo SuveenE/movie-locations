3a2bb50e7417386b0f1ddaa26fb50aded9bdc5a6
SuveenE <suveen.te1@gmail.com>
2024-06-10 22:06:10 +0800

    Add Singapore and Sri Lanka

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,35 @@
 import { useEffect, useState } from 'react';
 import Globe from 'react-globe.gl';
 import { Feature } from './utils/types';
+import geojsonData from './ne_110m_populated_places_simple.json'; 
 
 export default function Home() {
   const [places, setPlaces] = useState<Feature[]>([]);
 
   useEffect(() => {
-    fetch('../ne_110m_populated_places_simple.geojson')
-      .then((res) => res.json())
-      .then(({ features }) => setPlaces(features));
+    const fetchPlaces = async () => {
+      const { features } = await geojsonData; 
+      setPlaces(features as Feature[]); 
+    }
+
+    fetchPlaces();
+    
   }, []);
+
+  console.log(places)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      {/* <p className='p-4 '>Movie Locations Around the world</p> */}
         <Globe
           globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
           backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-          labelsData={places}
-          // labelLat={(d: Feature) => d.properties.latitude}
-          // labelLng={(d: Feature) => d.properties.longitude}
-          // labelText={(d: Feature) => d.properties.name}
-          // labelSize={(d: Feature) => Math.sqrt(d.properties.pop_max) * 4e-4}
-          // labelDotRadius={(d: Feature) => Math.sqrt(d.properties.pop_max) * 4e-4}
+          labelsData={places as unknown as Feature[]} // Type assertion here
+          labelLat={(d) => (d as Feature).properties.latitude}
+          labelLng={(d) => (d as Feature).properties.longitude}
+          labelText={(d) => (d as Feature).properties.sov0name}
+          labelSize={1}
+          labelDotRadius={1}
           labelColor={() => 'rgba(255, 165, 0, 0.75)'}
           labelResolution={2}
         />
