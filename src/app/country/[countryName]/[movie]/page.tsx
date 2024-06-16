2f1eb6bd85539ce06d032ab878a437e35a9196a2
SuveenE <suveen.te1@gmail.com>
2024-06-16 16:49:51 +0800

    Fix build errors

--- src/app/country/[countryName]/[movie]/page.tsx
+++ src/app/country/[countryName]/[movie]/page.tsx
@@ -1,8 +1,4 @@
 "use client";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Movie } from "@/utils/types";
 
 export interface Props {
   params: {
@@ -28,7 +24,7 @@ const CountryHome = ({ params }: Props) => {
   return (
     <div className="flex flex-col w-full font-mono">
       <p className=" m-2 p-2 mx-auto text-lg text-lime-300">
-        {params.movie}
+        Test
       </p>
       <div className="grid grid-cols-1 gap-3 md:grid-cols-4 w-fit mx-auto">
         </div>
