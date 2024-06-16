8219939ee0b8a8bd09a3258c84e9a8b631136914
SuveenE <suveen.te1@gmail.com>
2024-06-16 17:15:03 +0800

    Fix build errors

--- src/app/country/[countryName]/[movie]/page.tsx
+++ src/app/country/[countryName]/[movie]/page.tsx
@@ -6,31 +6,19 @@ export interface Props {
   };
 }
 
-const MovieHome = ({ params }: Props) => {
-//   const [movies, setMovies] = useState<Movie[]>([]);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const fetchMovies = async () => {
-//       const moviesList = await movieData;
-//       const movies = (moviesList as { [key: string]: Movie[] })[
-//         params.countryName
-//       ] as Movie[];
-//       setMovies(movies as Movie[]);
-//     };
-
-//     fetchMovies();
-//   }, [params.countryName]);
-  return (
-    <MoviePage params={params} />
-  );
-};
-
 export async function generateStaticParams() {
     const movieNames = ['SriLanka/bridge-on-the-river-kwai', 'Singapore/crazy-rich-asians']; 
     return movieNames.map((movieName) => ({
       movie: movieName,
     }));
   }
+
+const MovieHome = ({ params }: Props) => {
+  return (
+    <MoviePage params={params} />
+  );
+};
+
+
 
 export default MovieHome;
